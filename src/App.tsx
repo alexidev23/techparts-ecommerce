49b44d8def5c21f2df5cbc14a67b4a91efddcd9a
Alexis <alexisescobardev@gmail.com>
2026-03-10 10:29:22 -0300

    chore: add HelmetProvider to App root

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "@/app/router";
 import { AppProviders } from "@/app/providers";
 import { AuthProvider } from "./context/AuthContext";
+import { HelmetProvider } from "react-helmet-async";
 
 function App() {
   return (
-    <AuthProvider>
-      <AppProviders>
-        <RouterProvider router={router} />
-      </AppProviders>
-    </AuthProvider>
+    <HelmetProvider>
+      <AuthProvider>
+        <AppProviders>
+          <RouterProvider router={router} />
+        </AppProviders>
+      </AuthProvider>
+    </HelmetProvider>
   );
 }
 
